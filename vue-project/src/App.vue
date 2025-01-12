6396e1c9d03b16be8d999d93d14f30d10aedfe0a
jeonyul <jeonyul00@gmail.com>
2025-01-12 21:34:14 +0900

    update

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,28 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <AppHeader :app-title="message" @change="changeTitle" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import AppHeader from './components/AppHeader.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default defineComponent({
+  components: {
+    AppHeader,
+  },
+  setup() {
+    const message = ref('hello')
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    const changeTitle = (value: string) => {
+      message.value = value
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    return {
+      message,
+      changeTitle,
+    }
+  },
+})
+</script>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ vue-project/src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>header title ::: {{ appTitle }}</div>
+  <button @click="changeTitle">Change Title</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    appTitle: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(_, { emit }) {
+    const changeTitle = () => {
+      emit('change', 'updateTitle')
+    }
+
+    return {
+      changeTitle,
+    }
+  },
+})
+</script>
+
+<style scoped></style>
